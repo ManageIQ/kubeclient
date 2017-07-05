f1880db19281fa881281e85daa8997d055cd8b37
Daniel van Gils <daniel@cloud66.com>
2017-07-05 22:47:18 +0100

    update to deployments

--- test/test_deployment.rb
+++ test/test_deployment.rb
@@ -2,21 +2,21 @@ require 'test_helper'
 
 # Service entity tests
 class TestService < MiniTest::Test
-  def test_delete_a_entity_with_propagation_policy_query
+  def test_delete_a_deployment_with_propagation_policy_query
     value_propagation_policy = 'Foreground'
     namespace = 'my_namespace'
-    service = 'my_service'
+    deployment = 'my_deployment'
 
-    stub_request(:get, %r{/api/v1$})
-      .to_return(body: open_test_file('core_api_resource_list.json'), status: 200)
-    stub_request(:delete, %r{/namespaces/#{namespace}/services/#{service}})
+    stub_request(:get, %r{/api/extensions/v1beta1$})
+      .to_return(body: open_test_file('v1beta1_api_resource_list.json'), status: 200)
+    stub_request(:delete, %r{/namespaces/#{namespace}/deployments/#{deployment}})
       .to_return(body: open_test_file('entity_list.json'), status: 200)
 
-    client = Kubeclient::Client.new('http://localhost:8080/api/', 'v1')
+    client = Kubeclient::Client.new('http://localhost:8080/api/', 'extensions/v1beta1')
     query_hash = { propagationPolicy: value_propagation_policy }
-    client.delete_service(service, namespace, query: query_hash)
+    client.delete_deployment(deployment, namespace, query: query_hash)
 
-    expected_endpoint = "/api/v1/namespaces/#{namespace}/services/#{service}"
+    expected_endpoint = "/api/extensions/v1beta1/namespaces/#{namespace}/deployments/#{deployment}"
     expected_query = "?propagationPolicy=#{value_propagation_policy}"
     expected_url = "http://localhost:8080#{expected_endpoint}#{expected_query}"
 
