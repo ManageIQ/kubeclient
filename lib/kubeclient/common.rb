c352f53aae82bdc55f42dad014deeaa41f111d34
Ryan Rose <Ryan_Rose@bose.com>
2017-07-11 09:48:34 -0400

    add timestamps

--- lib/kubeclient/common.rb
+++ lib/kubeclient/common.rb
@@ -376,11 +376,12 @@ module Kubeclient
       end
     end
 
-    def watch_pod_log(pod_name, namespace, container: nil, tailLines: nil, sinceSeconds: nil)
+    def watch_pod_log(pod_name, namespace, container: nil, timestamps: true, tailLines: nil, sinceSeconds: nil)
       # Adding the "follow=true" query param tells the Kubernetes API to keep
       # the connection open and stream updates to the log.
       params = { follow: true }
       params[:container] = container if container
+      params[:timestamps] = timestamps if timestamps
       params[:sinceSeconds] = sinceSeconds if sinceSeconds
       params[:tailLines] = tailLines if tailLines
 
