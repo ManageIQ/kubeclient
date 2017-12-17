3fab5180e94df6fe13fa0d93662cee01576e5433
Federico Simoncelli <federico.simoncelli@gmail.com>
2017-12-17 19:15:20 +0200

    Merge pull request #279 from jhernand/recurse_over_arrays_for_watch_notices
    Recurse over arrays for watch notices
    
    (cherry picked from commit 893337516684a9e5f255013ef432e7750dda61d4)

---
+++ test/test_watch_notice.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class TestWatchNotice < MiniTest::Test
+  #
+  # Checks that elements of arrays are converted to instances of `RecursiveOpenStruct`, and that the
+  # items can be accessed using the dot notation.
+  #
+  def test_recurse_over_arrays
+    json = JSON.parse(open_test_file('node_notice.json').read)
+    notice = Kubeclient::Common::WatchNotice.new(json)
+    assert_kind_of(Array, notice.object.status.addresses)
+    notice.object.status.addresses.each do |address|
+      assert_kind_of(RecursiveOpenStruct, address)
+    end
+    assert_equal('InternalIP', notice.object.status.addresses[0].type)
+    assert_equal('192.168.122.40', notice.object.status.addresses[0].address)
+    assert_equal('Hostname', notice.object.status.addresses[1].type)
+    assert_equal('openshift.local', notice.object.status.addresses[1].address)
+  end
+
+  #
+  # Checks that even when arrays are converted to instances of `RecursiveOpenStruct` the items can
+  # be accessed using the hash notation.
+  #
+  def test_access_array_items_as_hash
+    json = JSON.parse(open_test_file('node_notice.json').read)
+    notice = Kubeclient::Common::WatchNotice.new(json)
+    assert_kind_of(Array, notice.object.status.addresses)
+    notice.object.status.addresses.each do |address|
+      assert_kind_of(RecursiveOpenStruct, address)
+    end
+    assert_equal('InternalIP', notice.object.status.addresses[0]['type'])
+    assert_equal('192.168.122.40', notice.object.status.addresses[0]['address'])
+    assert_equal('Hostname', notice.object.status.addresses[1]['type'])
+    assert_equal('openshift.local', notice.object.status.addresses[1]['address'])
+  end
+end
