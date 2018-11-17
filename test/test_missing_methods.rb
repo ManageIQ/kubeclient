fd6c2c1991204e1a89a47c6cebf79ff1b78622dd
Beni Cherniavsky-Paskin <cben@redhat.com>
2018-11-17 23:36:59 +0200

    Add tests for forced singular method names when kind is already plural
    security.openshift.io_api_resource_list.json taken from openshift v3.10.0.
    (openshift also includes SecurityContextConstraints also in api/v1/,
    but I prefer to keep core_api_resource_list.json pure upstream k8s
    and separate openshift-specific test stuff.)
    
    https://github.com/kubernetes/kubernetes/issues/8115

--- test/test_missing_methods.rb
+++ test/test_missing_methods.rb
@@ -29,11 +29,30 @@ class TestMissingMethods < MiniTest::Test
     ) # If discovery fails we expect the below raise an exception
     client = Kubeclient::Client.new('http://localhost:8080/api/', 'v1')
     assert_raises(Kubeclient::HttpError) do
+      client.discover
+    end
+    client = Kubeclient::Client.new('http://localhost:8080/api/', 'v1')
+    assert_raises(Kubeclient::HttpError) do
       client.method(:get_pods)
     end
     client = Kubeclient::Client.new('http://localhost:8080/api/', 'v1')
     assert_raises(Kubeclient::HttpError) do
       client.respond_to?(:get_pods)
     end
+  end
+
+  def test_irregular_names
+    stub_core_api_list
+    client = Kubeclient::Client.new('http://localhost:8080/api/', 'v1')
+    assert_equal(true, client.respond_to?(:get_endpoint))
+    assert_equal(true, client.respond_to?(:get_endpoints))
+
+    stub_request(:get, %r{/apis/security.openshift.io/v1$}).to_return(
+      body: open_test_file('security.openshift.io_api_resource_list.json'),
+      status: 200
+    )
+    client = Kubeclient::Client.new('http://localhost:8080/apis/security.openshift.io', 'v1')
+    assert_equal(true, client.respond_to?(:get_security_context_constraint))
+    assert_equal(true, client.respond_to?(:get_security_context_constraints))
   end
 end
