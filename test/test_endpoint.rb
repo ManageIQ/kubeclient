3ade15ad21a67c96fb7d7a476ac1e7374679b9ca
Beni Cherniavsky-Paskin <cben@redhat.com>
2018-11-21 18:04:25 +0200

    Test get_endpoints.kind - currently wrong forced singular
    https://github.com/abonas/kubeclient/issues/307

--- test/test_endpoint.rb
+++ test/test_endpoint.rb
@@ -4,7 +4,6 @@ require_relative 'test_helper'
 class TestEndpoint < MiniTest::Test
   def test_create_endpoint
     stub_core_api_list
-    client = Kubeclient::Client.new('http://localhost:8080/api/', 'v1')
     testing_ep = Kubeclient::Resource.new
     testing_ep.metadata = {}
     testing_ep.metadata.name = 'myendpoint'
@@ -24,7 +23,32 @@ class TestEndpoint < MiniTest::Test
       .with(body: req_body)
       .to_return(body: open_test_file('created_endpoint.json'), status: 201)
 
+    client = Kubeclient::Client.new('http://localhost:8080/api/', 'v1')
     created_ep = client.create_endpoint(testing_ep)
     assert_equal('Endpoints', created_ep.kind)
+    assert_equal('v1', created_ep.apiVersion)
+
+    client = Kubeclient::Client.new('http://localhost:8080/api/', 'v1', as: :parsed_symbolized)
+    created_ep = client.create_endpoint(testing_ep)
+    assert_equal('Endpoints', created_ep[:kind])
+    assert_equal('v1', created_ep[:apiVersion])
+  end
+
+  def test_get_endpoints
+    stub_core_api_list
+    stub_request(:get, %r{/endpoints})
+      .to_return(body: open_test_file('endpoint_list.json'), status: 200)
+    client = Kubeclient::Client.new('http://localhost:8080/api/', 'v1')
+
+    collection = client.get_endpoints(as: :parsed_symbolized)
+    assert_equal('EndpointsList', collection[:kind])
+    assert_equal('v1', collection[:apiVersion])
+
+    collection = client.get_endpoints
+    # TODO: this is wrong.  https://github.com/abonas/kubeclient/issues/307
+    # Kubernetes for single object uses kind: "Endpoints" (!) and
+    # kind: "EndpointsList" for plural.  While we force singular in method names
+    # to distinguish `get_endpoint` vs `get_endpoints`, we should not touch .kind.
+    assert_equal('Endpoint', collection.kind)
   end
 end
